d5113dbde0e02ede8eb9fd963b5c8a146707920f
Jury Alharbi <jury.abdulaziz@gmail.com>
2024-05-29 13:51:24 +0300

    edits on navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
   const role = decodeUser()?.role
 
   return (
-    <div className="flex justify-center mb-20">
+    <div className="w-full fixed top-0 z-50 flex justify-center mb-20">
       <NavigationMenu>
         <NavigationMenuList>
           <NavigationMenuItem>
