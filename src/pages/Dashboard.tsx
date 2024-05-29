344e9ac852e9fd451a6ca5c6dbe574ee737c1bdf
Jury Alharbi <jury.abdulaziz@gmail.com>
2024-05-29 15:00:31 +0300

    .

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -10,17 +10,15 @@ export default function Dashboard() {
       <Navbar />
       <Tabs defaultValue="products" className="mx auto">
         <TabsList className="flex justify-center mb-20">
-          <aside className="fixed left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
-            <TabsTrigger value="products" className="w-[400px]">
-              Products
-            </TabsTrigger>
-            <TabsTrigger value="categories" className="w-[400px]">
-              Categories
-            </TabsTrigger>
-            <TabsTrigger value="users" className="w-[400px]">
-              Users
-            </TabsTrigger>
-          </aside>
+          <TabsTrigger value="products" className="w-[400px]">
+            Products
+          </TabsTrigger>
+          <TabsTrigger value="categories" className="w-[400px]">
+            Categories
+          </TabsTrigger>
+          <TabsTrigger value="users" className="w-[400px]">
+            Users
+          </TabsTrigger>
         </TabsList>
         <TabsContent value="products">
           <Product />
