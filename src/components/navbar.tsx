49e8592d95153e73aacb1e762aadcb6d5690e5b0
Jury Alharbi <jury.abdulaziz@gmail.com>
2024-05-29 13:56:00 +0300

    edits on navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -13,6 +13,7 @@ import { GlobalContext } from "@/App"
 import { ROLE } from "@/types"
 import { decodeUser } from "@/lib/utils"
 import { ModeToggle } from "./mode-toggle"
+import { Card } from "./ui/card"
 
 export default function Navbar() {
   const context = useContext(GlobalContext)
@@ -23,69 +24,73 @@ export default function Navbar() {
 
   return (
     <div className="w-full fixed top-0 z-50 flex justify-center mb-20">
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <Link to="/">
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>Home</NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          {role === ROLE.Admin && (
-            <NavigationMenuItem>
-              <Link to="/Dashboard">
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Dashboard
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-          )}
-          {role === ROLE.User && (
-            <NavigationMenuItem>
-              <Link to="/Cart">
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Cart
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-          )}
-          {role === ROLE.User && (
-            <NavigationMenuItem>
-              <Link to="/profile">
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Profile
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-          )}
-          {state.user ? (
+      <Card className="w-full fixed top-0 z-50 flex justify-center mb-20">
+        <NavigationMenu>
+          <NavigationMenuList>
             <NavigationMenuItem>
               <Link to="/">
-                <NavigationMenuLink
-                  className={navigationMenuTriggerStyle()}
-                  onClick={() => {
-                    localStorage.removeItem("token")
-                    localStorage.removeItem("user")
-                    handleLogout()
-                  }}
-                >
-                  Logout
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-          ) : (
-            <NavigationMenuItem>
-              <Link to="/login">
                 <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Login
+                  Home
                 </NavigationMenuLink>
               </Link>
             </NavigationMenuItem>
-          )}
-          <NavigationMenuItem>
-            <ModeToggle />
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+            {role === ROLE.Admin && (
+              <NavigationMenuItem>
+                <Link to="/Dashboard">
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Dashboard
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            )}
+            {role === ROLE.User && (
+              <NavigationMenuItem>
+                <Link to="/Cart">
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Cart
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            )}
+            {role === ROLE.User && (
+              <NavigationMenuItem>
+                <Link to="/profile">
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Profile
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            )}
+            {state.user ? (
+              <NavigationMenuItem>
+                <Link to="/">
+                  <NavigationMenuLink
+                    className={navigationMenuTriggerStyle()}
+                    onClick={() => {
+                      localStorage.removeItem("token")
+                      localStorage.removeItem("user")
+                      handleLogout()
+                    }}
+                  >
+                    Logout
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            ) : (
+              <NavigationMenuItem>
+                <Link to="/login">
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Login
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            )}
+            <NavigationMenuItem>
+              <ModeToggle />
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </Card>
     </div>
   )
 }
