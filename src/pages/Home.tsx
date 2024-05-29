fbc31b9b1efbf2083efee60e66974bc25be1815e
Jury Alharbi <jury.abdulaziz@gmail.com>
2024-05-29 14:38:13 +0300

    hehee

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query"
+import { useQuery, useQueryClient } from "@tanstack/react-query"
 import { ChangeEvent, useContext, useState } from "react"
 import { Link } from "react-router-dom"
 
